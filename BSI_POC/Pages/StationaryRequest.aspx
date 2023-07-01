65a5f087eadde3a8b962ec6d1bd6ac28e86bcb68
ramadhaniabidin12@gmail.com <ramadhaniabidin12@gmail.com>
2023-07-01 07:00:32 +0700

    update

--- BSI_POC/Pages/StationaryRequest.aspx
+++ BSI_POC/Pages/StationaryRequest.aspx
@@ -1,3 +1,7 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="StationaryRequest.aspx.cs" Inherits="BSI_POC.Pages.StationaryRequest" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
 </asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h1>Stationary Request</h1>
+</asp:Content>
--- BSI_POC/Site.Master.designer.cs
+++ BSI_POC/Site.Master.designer.cs
@@ -22,5 +22,14 @@ namespace BSI_POC
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
+
+        /// <summary>
+        /// ContentPlaceHolder1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
     }
 }
