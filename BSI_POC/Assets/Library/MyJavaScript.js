b5183c8040d67585fd334c8635403400303aeda6
ramadhaniabidin12@gmail.com <ramadhaniabidin12@gmail.com>
2023-07-02 22:04:25 +0700

    updates

--- BSI_POC/Assets/Library/MyJavaScript.js
+++ BSI_POC/Assets/Library/MyJavaScript.js
@@ -1,47 +1,56 @@
 ﻿function addRow() {
     const el = document.getElementById("add");
-        const table = document.getElementById("semprot_lahan");
-        const newRow = table.insertRow();
+    const table = document.getElementById("semprot_lahan");
+    var rows = table.getElementsByTagName("tr");
+    var rowCount = rows.length;
+    for (var i = 1; i < rowCount; i++) {
+        rows[i].cells[0].innerHTML = i;
+    }
 
-        const cell1 = newRow.insertCell();
-        const cell2 = newRow.insertCell();
-        const cell3 = newRow.insertCell();
-        const cell4 = newRow.insertCell();
-        const cell5 = newRow.insertCell();
+    const newRow = table.insertRow();
+    const cell1 = newRow.insertCell();
+    const cell2 = newRow.insertCell();
+    const cell3 = newRow.insertCell();
+    const cell4 = newRow.insertCell();
+    const cell5 = newRow.insertCell();
 
-        const dropdown = document.createElement('select');
+    var noText = document.createTextNode(rowCount);
 
-        const option0 = document.createElement('option');
-        option0.text = '== Select Stationary Item ==';
-        option0.disabled = "disabled";
-        option0.selected = "selected";
-        option0.classList.add("text-center");
-        dropdown.add(option0);
+    cell1.appendChild(noText);
 
-        const option1 = document.createElement('option');
-        option1.text = 'A4 Paper';
-        dropdown.add(option1);
+    const dropdown = document.createElement('select');
 
-        const option2 = document.createElement('option');
-        option2.text = 'Pencil';
-        dropdown.add(option2);
+    const option0 = document.createElement('option');
+    option0.text = '== Select Stationary Item ==';
+    option0.disabled = "disabled";
+    option0.selected = "selected";
+    option0.classList.add("text-center");
+    dropdown.add(option0);
 
-        const option3 = document.createElement('option');
-        option3.text = 'Pencil';
-        dropdown.add(option3);
+    const option1 = document.createElement('option');
+    option1.text = 'A4 Paper';
+    dropdown.add(option1);
 
-        const option4 = document.createElement('option');
-        option4.text = 'Marker';
-        dropdown.add(option4);
+    const option2 = document.createElement('option');
+    option2.text = 'Pencil';
+    dropdown.add(option2);
 
-        const option5 = document.createElement('option');
-        option5.text = 'Envelope';
-        dropdown.add(option5);
+    const option3 = document.createElement('option');
+    option3.text = 'Pencil';
+    dropdown.add(option3);
+
+    const option4 = document.createElement('option');
+    option4.text = 'Marker';
+    dropdown.add(option4);
+
+    const option5 = document.createElement('option');
+    option5.text = 'Envelope';
+    dropdown.add(option5);
 
     dropdown.style.width = "90%";
 
 
-    const celss = [cell1, cell3, cell4];
+    const celss = [cell3, cell4];
 
     cell2.appendChild(dropdown);
     for (const i of celss) {
@@ -49,7 +58,6 @@
         inp.type = "text";
         inp.style.width = "95%";
         i.appendChild(inp);
-        console.log(i);
     }
 
     const txt = document.createElement('textarea');
@@ -63,6 +71,7 @@
         console.log(inputBox);
     })
     cell5.appendChild(txt);
+
 
 }
 
