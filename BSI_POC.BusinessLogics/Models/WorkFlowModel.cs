68a5a405729c7e1c319f19302c70409a3e2b930d
ramadhaniabidin12@gmail.com <ramadhaniabidin12@gmail.com>
2023-07-07 17:26:45 +0700

    Update --Create WorkFlowModel.cs

---
+++ BSI_POC.BusinessLogics/Models/WorkFlowModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSI_POC.BusinessLogics.Models
+{
+    public class ResponseWorkflow
+    {
+        public string id { get; set; }
+    }
+    public class NintexWorkflowCloud
+    {
+        public string url { get; set; }
+        public NWCParamModel param { get; set; }
+    }
+
+    public class NWCParamModel
+    {
+        public StartData startData { get; set; }
+        //public Options options { get; set; }
+    }
+    public class StartData
+    {
+        public int se_transactionid { get; set; }
+
+    }
+    public class Options
+    {
+        public string callbackUrl { get; set; }
+        public string instanceToken { get; set; }
+    }
+
+
+}
